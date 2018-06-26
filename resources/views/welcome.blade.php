afcfe94d84668585489b99d453e0f0bd769ef95a
BasLaravel <38691103+BasLaravel@users.noreply.github.com>
2018-06-26 13:36:48 +0200

    background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,30 +13,52 @@
 
         <!-- Styles -->
         <style>
-       
+         .videoContainer {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                top: 0;
+                left: 0;
+                bottom: 0;
+                right: 0;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                z-index:-1;
+                }
+
+                iframe {
+                /* optional */
+                width: 100%;
+                height: 100%; 
+                }
+
+      
         </style>
     </head>
     <body>
 
-  
+<!-- <iframe style="position: absolute; top: 0; left: 0;"
+src="https://www.youtube.com/embed/8HSr8BjcufM?autoplay=1&controls=0&loop=1&playlist=8HSr8BjcufM&amp;showinfo=0" 
+width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> -->
  
+
+<div class="videoContainer">
+        <iframe width="1920" height="1080" 
+        src="https://www.youtube.com/embed/videoseries?list=PLCPhqEUKuUo7lAtXkSXQyNtBH5xBNhL_m&autoplay=1&loop=1&controls=0&showinfo=0"
+        frameborder="0"></iframe> 
+   </div>  
+
 
 
 
    
-<div class="container">
-    <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="/images/attractions.jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-  </div>  
-  
-  <iframe width="420" height="350" src="https://www.youtube.com/embed/WSUDSMZNjEs&index=14&list=PLj6XzcqwRpN4CVMKhTUQi1jh36H3uSMnM"></iframe>
+
 
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+
     </body>
+
 </html>
