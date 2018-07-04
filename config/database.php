d73d7f6c708a798710c456ae551147c402b40ad9
BasLaravel <38691103+BasLaravel@users.noreply.github.com>
2018-07-04 14:47:36 +0200

    db

--- config/database.php
+++ config/database.php
@@ -78,6 +78,19 @@ return [
             'prefix' => '',
         ],
 
+        'pg-heroku' => [
+            'driver' => 'pgsql',
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'require',
+        ],
+
     ],
 
     /*
