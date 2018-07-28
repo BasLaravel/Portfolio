f4062423bbc939a8e614f0f7b261cc368522558d
BasLaravel <38691103+BasLaravel@users.noreply.github.com>
2018-07-28 10:46:25 +0200

    link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -72,6 +72,12 @@
                 margin-right:auto;
             }
 
+            #linken a img:hover {
+                -ms-transform: scale(1.5); /* IE 9 */
+                -webkit-transform: scale(1.5); /* Safari 3-8 */
+                transform: scale(1.5); 
+            }
+
             #profieltekst{
                 display: flex;
             }
